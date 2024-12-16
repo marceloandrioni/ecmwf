80bf26770724cd8114b42473562bd460e8c190d1
Marcelo Andrioni <marceloandrioni@gmail.com>
2024-12-16 09:25:13 -0300

    add wave variables

--- ecmwf_era5_get.sh
+++ ecmwf_era5_get.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dt_start="20000101"
+dt_start="19900101"
 dt_stop="20231231"
 time_delta="1 month"
 
@@ -12,9 +12,86 @@ extent=(-54 -31 -36 7)
 
 outdir_base="/u/eani/operational/datasets/ecmwf/era5_new/$region/raw"
 
-# group "name" and list of variables
-gvar="wind10"
-vars=("10m_u_component_of_wind" "10m_v_component_of_wind")
+# group variable "name"
+gvar="wave_part1"
+
+case $gvar in
+    "bathymetry")
+        # non-time varying
+        vars=("model_bathymetry"
+              "land_sea_mask")
+        ;;
+    "wind10")
+        vars=("10m_u_component_of_wind"
+              "10m_v_component_of_wind")
+        ;;
+    "wind100")
+        vars=("100m_u_component_of_wind"
+              "100m_v_component_of_wind")
+        ;;
+
+    # ERA5: How to calculate Obukhov Length
+    # https://confluence.ecmwf.int/display/CKB/ERA5%3A+How+to+calculate+Obukhov+Length
+    # "surface_pressure"
+    # "2m_dewpoint_temperature"
+    # "2m_temperature"
+    # "instantaneous_eastward_turbulent_surface_stress"
+    # "instantaneous_moisture_flux"
+    # "instantaneous_northward_turbulent_surface_stress"
+    # "instantaneous_surface_sensible_heat_flux"
+    # "standard_deviation_of_filtered_subgrid_orography"
+
+    "wave_part1")
+        # Hs
+        vars=("significant_height_of_combined_wind_waves_and_swell"
+              "significant_height_of_total_swell"
+              "significant_height_of_wind_waves")
+        ;;
+    "wave_part2")
+        # Tp and Tm-1,0
+        vars=("peak_wave_period"
+              "mean_wave_period"
+              "mean_period_of_total_swell"
+              "mean_period_of_wind_waves")
+        ;;
+    "wave_part3")
+        # Tm0,1
+        vars=("mean_wave_period_based_on_first_moment"
+              "mean_wave_period_based_on_first_moment_for_swell"
+              "mean_wave_period_based_on_first_moment_for_wind_waves")
+        ;;
+    "wave_part4")
+        # Tm0,2
+        vars=("mean_zero_crossing_wave_period"
+              "mean_wave_period_based_on_second_moment_for_swell"
+              "mean_wave_period_based_on_second_moment_for_wind_waves")
+        ;;
+    "wave_part5")
+        # Direction
+        vars=("mean_wave_direction"
+              "mean_direction_of_total_swell"
+              "mean_direction_of_wind_waves")
+        ;;
+    "wave_part6")
+        # Directional width to calculate spreading
+        vars=("wave_spectral_directional_width"
+              "wave_spectral_directional_width_for_swell"
+              "wave_spectral_directional_width_for_wind_waves")
+        ;;
+    "wave_part7")
+        # maximum individual wave
+        vars=("maximum_individual_wave_height"
+              "period_corresponding_to_maximum_individual_wave_height")
+        ;;
+    "wave_part8")
+        # Stokes
+        vars=("u_component_stokes_drift"
+              "v_component_stokes_drift")
+        ;;
+    *)
+        echo "Invalid group variable $gvar"
+        exit 1
+esac
 
 dt="$dt_start"
 while [[ $dt -le $dt_stop ]]; do
